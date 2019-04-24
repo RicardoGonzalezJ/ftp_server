827ce2fa8765320e0e5261bbbd45939708427f5c
RicardoGonzalezJ <rgonzalezj1480@gmail.com>
2019-04-23 23:43:15 -0500

    improved the code

--- ftpserv.js
+++ ftpserv.js
@@ -1,14 +1,40 @@
-'use strict';
 
-const
-  net = require('net'),
-  fs = require('fs'),
-  port = process.argv[2],
-  spawn = require('child_process').spawn,
+const net = require('net');
+const fs = require('fs');
 
-  server = net.createServer(function (socket) {
+const port = process.argv[2];
+const { spawn } = require('child_process');
 
-  //reporting
+function commandManager(cmnd) {
+  const
+    command = cmnd.trim().toLowerCase();
+  const cleanCommand = (command === 'pwd' || command === 'ls' || command === '@quit') ? command : command.substring(0, command.indexOf(' '));
+  let dirname = '';
+  let filename = '';
+  const parameters = [];
+
+  if (cleanCommand === 'pwd' || cleanCommand === 'ls' || command === '@quit') {
+    parameters.push(cleanCommand);
+    return parameters;
+  }
+  if (cleanCommand === 'cd') {
+    dirname = command.substring(command.indexOf(' ') + 1);
+    parameters.push(cleanCommand);
+    parameters.push(dirname);
+  } else if (cleanCommand === 'mkdir' || cleanCommand === 'rmdir') {
+    dirname = command.substring(command.indexOf(' ') + 1);
+    parameters.push(cleanCommand);
+    parameters.push(dirname);
+  } else if (cleanCommand === 'get' || cleanCommand === 'put') {
+    filename = command.substring(command.indexOf(' ') + 1);
+    parameters.push(cleanCommand);
+    parameters.push(filename);
+  }
+  return parameters;
+}
+
+const server = net.createServer((socket) => {
+  // reporting
   console.log('Client connected');
   console.log(`localAddress: ${socket.localAddress}`);
   console.log(`RemoteAddress: ${socket.remoteAddress}`);
@@ -16,131 +42,100 @@ const
   socket.setEncoding('utf8');
 
   // commands Available
-  socket.write('Commands Available:\npwd\n'+'cd\n'+'ls\n'+'mkdir\n'+'rmdir\n'+'get\n'+'put\n'+'@quit\n\n');
+  socket.write('Commands Available:\npwd\n' + 'cd\n' + 'ls\n' + 'mkdir\n' + 'rmdir\n' + 'get\n' + 'put\n' + '@quit\n\n');
 
-  let ftpServer = function (arg) {
-
-    let commands = commandManager(arg);
+  const ftpServer = function (arg) {
+    const commands = commandManager(arg);
 
     switch (commands[0]) {
       case 'pwd':
-        socket.write(process.cwd() + '\n');
-        return ;
+        socket.write(`${process.cwd()}\n`);
+        break;
       case 'cd':
         process.chdir(commands[1]);
-        return ;
-      case 'ls':
-      const
-        ls = spawn('ls', [`${process.cwd()}`]);
+        break;
+      case 'ls': {
+        const
+          ls = spawn('ls', [`${process.cwd()}`]);
 
-        ls.stdout.on('data', function (data) {
-          socket.write(data.toString());
-        })
-
-        ls.stderr.on('data', function (data) {
+        ls.stdout.on('data', (data) => {
           socket.write(data.toString());
         });
-        return ;
+
+        ls.stderr.on('data', (data) => {
+          socket.write(data.toString());
+        });
+        break;
+      }
       case 'mkdir':
-        fs.mkdir(commands[1], function (err) {
+        fs.mkdir(commands[1], (err) => {
           if (err) {
             socket.write(`${err.message}\n`);
-          }else {
+          } else {
             socket.write(`directory ${commands[1]} was created.\n`);
           }
         });
-        return ;
+        break;
       case 'rmdir':
-        fs.rmdir(commands[1], function (err) {
+        fs.rmdir(commands[1], (err) => {
           if (err) {
             socket.write(`${err.message}\n`);
-          }else {
+          } else {
             socket.write(`directory ${commands[1]} was deleted.\n`);
           }
         });
-        return ;
+        break;
       case 'get':
-      fs.copyFile(`${process.cwd()}/${commands[1]}`, `${process.env.OLDPWD}/ftp_client/local/${commands[1]}`, (err)=>{
-        if (err) {
-          socket.write(`${err.message}\n`);
-        }else {
-          socket.write(`file ${commands[1]} was copied succesfully\n`);
-        }
-
-      });
-       return ;
-       case 'put':
-       fs.copyFile(`${process.env.OLDPWD}/ftp_client/local/${commands[1]}`,`${process.cwd()}/remote/${commands[1]}`, (err)=>{
-         if (err) {
-           socket.write(`${err.message}\n`);
-         }else {
-           socket.write(`file ${commands[1]} was copied succesfully\n`);
-         }
-       });
-        return ;
+        fs.copyFile(`${process.cwd()}/${commands[1]}`, `${process.env.OLDPWD}/ftp_client/local/${commands[1]}`, (err) => {
+          if (err) {
+            socket.write(`${err.message}\n`);
+          } else {
+            socket.write(`file ${commands[1]} was copied succesfully\n`);
+          }
+        });
+        break;
+      case 'put':
+        fs.copyFile(`${process.env.OLDPWD}/ftp_client/local/${commands[1]}`, `${process.cwd()}/remote/${commands[1]}`, (err) => {
+          if (err) {
+            socket.write(`${err.message}\n`);
+          } else {
+            socket.write(`file ${commands[1]} was copied succesfully\n`);
+          }
+        });
+        break;
       case '@quit':
-        return socket.end('Bye\n');
+        socket.end('Bye\n');
+        break;
+      default:
+        socket.write('invalid command');
+        break;
     }
-  }
+  };
 
-  socket.on('data', function (data) {
-    ftpServer(data)
-  })
-
-  socket.on('end', function () {
-    console.log('client connection ended.');
+  socket.on('data', (data) => {
+    ftpServer(data);
   });
 
+  socket.on('end', () => {
+    console.log('client connection ended.');
+  });
 });
 
-server.on('listening', function () {
+server.on('listening', () => {
   console.log(`Server is running on port ${port}`);
+});
 
-})
-
-server.on('error', function (error) {
+server.on('error', (error) => {
   console.log(`error server ${error.message}`);
 });
 
-server.on('close', function () {
+server.on('close', () => {
   console.log('Server is closed.');
-})
+});
 
 // message in case port is missing
 if (port === undefined) {
-  console.log(`Please include port number as 'node ftpserv.js port'`);
-}else {
+  console.log('Please include port number as \'node ftpserv.js port\'');
+} else {
   server.listen(port);
-}
-
-function commandManager(cmnd) {
-
-let
-  command = cmnd.trim().toLowerCase(),
-  cleanCommand = (command === 'pwd' || command === 'ls' || command === '@quit') ? command : command.substring(0,command.indexOf(" ")),
-  dirname = '',
-  filename = '',
-  parameters = [];
-
-if (cleanCommand === 'pwd' || cleanCommand === 'ls' || command === '@quit') {
-  parameters.push(cleanCommand);
-  return parameters;
-}
-if (cleanCommand === 'cd') {
-  dirname = command.substring(command.indexOf(" ")+1);
-  parameters.push(cleanCommand);
-  parameters.push(dirname);
-
-}else if (cleanCommand === 'mkdir' || cleanCommand === 'rmdir') {
-  dirname = command.substring(command.indexOf(" ")+1);
-  parameters.push(cleanCommand);
-  parameters.push(dirname);
-
-}else if (cleanCommand === 'get' || cleanCommand === 'put') {
-  filename = command.substring(command.indexOf(" ")+1);
-  parameters.push(cleanCommand);
-  parameters.push(filename);
-
-}
- return parameters;
 }
